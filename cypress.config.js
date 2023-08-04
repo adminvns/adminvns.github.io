aed11822d62e71bbb68d46b5df1c2122db27654f
adminvns <shubham.editor@gmail.com>
2023-08-04 14:27:20 +0530

    updated  env secret

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '${{ secrets.CYPRESS_PROJECT_KEY }}',
+  projectId: '1of8yz',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
